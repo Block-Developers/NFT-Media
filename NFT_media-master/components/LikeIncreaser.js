21570afb67bfeadddd27de3368d7257b1c3ce6d2
thirumurugan7 <thiru82003@gmail.com>
2022-12-14 06:26:20 +0530

    working onBE

--- NFT_media-master/components/LikeIncreaser.js
+++ NFT_media-master/components/LikeIncreaser.js
@@ -1,5 +1,12 @@
 import { useWeb3Contract } from "react-moralis";
-
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
 export default function LikePlus() {
+  const { runContractFunction: likeplus } = useWeb3Contract({
+    abi: abi,
+    contractAddress: contractAddresses, //need to specify the chain id
+    functionName: "likeplus",
+    params: {},
+  });
   return <div>like checking</div>;
 }
--- backend/deploy/update-frontend.js
+++ backend/deploy/update-frontend.js
@@ -1,19 +1,39 @@
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
 const fs = require("fs");
 const FRONT_END_ADDRESS_FILE =
-  "../../NFT_media-master/constants/contractAddresses.json";
-const FRONT_END_ABI_FILE = "../../NFT_media-master/constants/abi.json";
+  "../NFT_media-master/constants/contractAddresses.json";
+const FRONT_END_ABI_FILE = "../NFT_media-master/constants/abi.json";
 
 module.exports = async function () {
   if (process.env.UPDATE_FRONT_END) {
     console.log("Updating frontend");
     updateContractAddresses();
+    updateAbi();
   }
 };
 
+async function updateAbi() {
+  const nftmedia = await ethers.getContract("NftMedia");
+  fs.writeFileSync(
+    FRONT_END_ABI_FILE,
+    nftmedia.interface.format(ethers.utils.FormatTypes.json)
+  );
+}
+
 async function updateContractAddresses() {
   const nftmedia = await ethers.getContract("NftMedia");
+  const chainId = network.config.chainId.toString();
   const currentAddress = JSON.parse(
     fs.readFileSync(FRONT_END_ADDRESS_FILE, "utf8")
   );
+  if (network.config.chainId.toString() in currentAddress) {
+    if (!currentAddress[chainId].includes(nftmedia.address)) {
+      currentAddress[chainId].push(nftmedia.address);
+    }
+  }
+  {
+    currentAddress[chainId] = [nftmedia.address];
+  }
+  fs.writeFileSync(FRONT_END_ADDRESS_FILE, JSON.stringify(currentAddress));
 }
+module.exports.tags = ["all", "frontend"];
